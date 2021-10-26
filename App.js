6a1bcd0f4bf95df8a263ca414332bb807456fd03
tony <antolisah@gmail.com>
2021-10-26 16:47:09 +0300

    flatlist and listitem components

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import ListItem from './components/ListItem';
+
+import { SAMPLE_DATA } from './assets/data/sampleData';
+
+const ListHeader = () => (
+  <>
+    <View style={styles.titleWrapper}>
+      <Text style={styles.largeTitle}>Markets</Text>
+    </View>
+    <View style={styles.divider} />
+  </>
+)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={SAMPLE_DATA}
+        renderItem={({ item }) => (
+          <ListItem
+            name={item.name}
+            symbol={item.symbol}
+            logoUrl={item.image}
+            currentPrice={item.current_price}
+            priceChangePercentage7d={item.price_change_percentage_7d_in_currency}
+          />
+        )}
+        keyExtractor={(item) => item.id}
+        ListHeaderComponent={ListHeader}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +38,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  titleWrapper: {
+    marginTop: 20,
+    padding: 16,
+  },
+  largeTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  divider: {
+    height: 1,
+    backgroundColor: '#eaeaea',
+    marginHorizontal: 16,
+    marginTop: 16,
   },
 });
---
+++ components/ListItem.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native'
+
+const ListItem = ({ name, symbol, currentPrice, priceChangePercentage7d, logoUrl }) => {
+  const priceChangeColor = priceChangePercentage7d > 0 ? '#34c759' : '#ff3b30'
+  return (
+    <TouchableOpacity>
+      <View style={styles.itemWrapper}>
+        <View style={styles.leftWrapper}>
+          <Image style={styles.image} source={{ uri: logoUrl }} />
+          <View style={styles.titleWrapper}>
+            <Text style={styles.title}>{name}</Text>
+            <Text style={styles.subtitle}>{symbol.toUpperCase()}</Text>
+          </View>
+        </View>
+        <View style={styles.rightWrapper}>
+          <Text style={styles.price}>${currentPrice.toLocaleString('en-US', { currency: 'USD' })}</Text>
+          <Text style={[styles.percentage, { color: priceChangeColor }]}>{priceChangePercentage7d.toFixed(2)}%</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  itemWrapper: {
+    paddingHorizontal: 16,
+    marginTop: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  leftWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25
+  },
+  titleWrapper: {
+    marginLeft: 8
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#888'
+  },
+  subtitle: {
+    marginTop: 4,
+    fontSize: 14,
+    color: '#ccc',
+  },
+  rightWrapper: {
+    alignItems: 'flex-end'
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  percentage: {
+    marginTop: 4,
+    fontSize: 14,
+    color: 'red'
+  }
+})
+
+export default ListItem
