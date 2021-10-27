88b2bfa45ade7138dfc541f303aeb60da9017d9c
tony <antolisah@gmail.com>
2021-10-27 04:36:12 +0300

    chart view

--- components/Chart.js
+++ components/Chart.js
@@ -1,34 +1,46 @@
 import React from 'react'
-import { StyleSheet, View, Text, Image } from 'react-native'
+import { StyleSheet, View, Text, Image, Dimensions } from 'react-native'
+import { ChartDot, ChartPath, ChartPathProvider } from '@rainbow-me/animated-charts';
+
+export const { width: SIZE } = Dimensions.get('window');
 
 const Chart = ({ name, logoUrl, symbol, currentPrice, priceChangePercentage7d, sparklineData }) => {
   const priceChangeColor = priceChangePercentage7d > 0 ? '#34c759' : '#ff3b30'
 
   return (
-    <View style={styles.chartWrapper}>
-      <View style={styles.titlesWrapper}>
-        <View style={styles.upper}>
-          <View style={styles.leftWrapper}>
-            <Image source={{ uri: logoUrl }} style={styles.image} />
-            <Text style={styles.subtitle}>{name} ({symbol.toUpperCase()})</Text>
+    <ChartPathProvider data={{ points: sparklineData, smoothingStrategy: 'bezier' }}>
+      <View style={styles.chartWrapper}>
+        <View style={styles.titlesWrapper}>
+          <View style={styles.upper}>
+            <View style={styles.leftWrapper}>
+              <Image source={{ uri: logoUrl }} style={styles.image} />
+              <Text style={styles.subtitle}>{name} ({symbol.toUpperCase()})</Text>
+            </View>
+            <Text style={styles.subtitle}>7d</Text>
           </View>
-          <Text style={styles.subtitle}>7d</Text>
+
+          <View style={styles.lower}>
+            <Text style={styles.price}>${currentPrice.toLocaleString('en-US', { currency: 'USD' })}</Text>
+            <Text style={[styles.percentage, { color: priceChangeColor }]}>{priceChangePercentage7d.toFixed(2)}%</Text>
+          </View>
         </View>
 
-        <View style={styles.lower}>
-          <Text style={styles.price}>${currentPrice.toLocaleString('en-US', { currency: 'USD' })}</Text>
-          <Text style={[styles.percentage, { color: priceChangeColor }]}>{priceChangePercentage7d.toFixed(2)}%</Text>
+        <View style={styles.chartLineWrapper}>
+          <ChartPath height={SIZE / 2} stroke="black" width={SIZE} />
+          <ChartDot style={{ backgroundColor: 'blue' }} />
         </View>
       </View>
-    </View>
+    </ChartPathProvider>
   )
 }
 
 const styles = StyleSheet.create({
   chartWrapper: {
-    margin: 16,
+    marginVertical: 16,
   },
-  titlesWrapper: {},
+  titlesWrapper: {
+    marginHorizontal: 16,
+  },
   upper: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -59,6 +71,9 @@ const styles = StyleSheet.create({
   },
   percentage: {
     fontSize: 18,
+  },
+  chartLineWrapper: {
+    marginTop: 40,
   },
 })
 
